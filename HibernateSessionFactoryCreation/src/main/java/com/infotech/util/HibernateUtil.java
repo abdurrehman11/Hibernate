31bdf1dbf6195ac4e819c46f009f9ff6913c967b
Rizwan Majeed <rizwan.majeed@venturedive.com>
2018-09-14 16:42:39 +0500

    Update hibernate.util file

--- HibernateSessionFactoryCreation/src/main/java/com/infotech/util/HibernateUtil.java
+++ HibernateSessionFactoryCreation/src/main/java/com/infotech/util/HibernateUtil.java
@@ -1,20 +1,26 @@
 package com.infotech.util;
 
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class HibernateUtil {
 
+    private  static StandardServiceRegistry standardServiceRegistry;
     private static SessionFactory sessionFactory;
 
     static {
-        try {
-            if (sessionFactory == null) {
-                Configuration config = new Configuration();
-                sessionFactory = config.configure().buildSessionFactory();
+        if (sessionFactory == null) {
+            try {
+                standardServiceRegistry = new StandardServiceRegistryBuilder().configure().build();
+                MetadataSources metadataSources = new MetadataSources(standardServiceRegistry);
+                Metadata metadata = metadataSources.getMetadataBuilder().build();
+                sessionFactory = metadata.getSessionFactoryBuilder().build();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
