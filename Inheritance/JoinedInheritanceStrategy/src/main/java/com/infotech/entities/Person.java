e0f0ee141b0b3d3fff1c5699011a4eb5a587fd80
Abdur Rehman <rizwan.majeed@venturedive.com>
2018-09-18 12:55:23 +0500

    Joined Inheritance Strategy example

---
+++ Inheritance/JoinedInheritanceStrategy/src/main/java/com/infotech/entities/Person.java
@@ -0,0 +1,44 @@
+package com.infotech.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "person_table")
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name", length = 100)
+    private String name;
+
+    @Column(name = "gender", length = 10)
+    private String gender;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
---
+++ Inheritance/JoinedInheritanceStrategy/src/main/java/com/infotech/entities/Student.java
@@ -0,0 +1,53 @@
+package com.infotech.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "student_table")
+public class Student extends Person {
+
+    @Column(name = "school_name", length = 50)
+    private String schoolName;
+
+    @Column(name = "section_name", length = 50)
+    private String sectionName;
+
+    @Column(name = "fee")
+    private Float fee;
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public String getSectionName() {
+        return sectionName;
+    }
+
+    public void setSectionName(String sectionName) {
+        this.sectionName = sectionName;
+    }
+
+    public Float getFee() {
+        return fee;
+    }
+
+    public void setFee(Float fee) {
+        this.fee = fee;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "schoolName='" + schoolName + '\'' +
+                ", sectionName='" + sectionName + '\'' +
+                ", fee=" + fee +
+                '}';
+    }
+
+}
