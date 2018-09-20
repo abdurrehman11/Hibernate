ee3c63ebf01e5f1428b5b84fe35c576f02904a87
Abdur Rehman <rizwan.majeed@venturedive.com>
2018-09-20 17:56:53 +0500

    updated HQL for OneToOne Mapping

--- HQL/OneToOneMapping HQL/src/main/java/com/infotech/client/HQLClient.java
+++ HQL/OneToOneMapping HQL/src/main/java/com/infotech/client/HQLClient.java
@@ -7,12 +7,16 @@ import org.hibernate.Session;
 
 import org.hibernate.query.Query;
 
+import java.util.Date;
+import java.util.List;
+
 public class HQLClient {
 
     public static void main(String[] args) {
 
 //        getEmployeeAndAdressByEmployeeId();
-        getEmployeeAndAdressByAddressId();
+//        getEmployeeAndAdressByAddressId();
+        getEmployeeAndAdressFieldsByAddressId();
     }
 
     private static void getEmployeeAndAdressByEmployeeId() {
@@ -40,6 +44,27 @@ public class HQLClient {
             System.out.println(address);
             Employee employee = address.getEmployee();
             System.out.println(employee);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void getEmployeeAndAdressFieldsByAddressId() {
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            String HQL = "SELECT emp.employeeName, emp.doj, emp.salary, addr.city, addr.pincode FROM Employee emp LEFT JOIN emp.address addr WHERE emp.employeeId = :empId";
+            Query<Object []> query = session.createQuery(HQL);
+            query.setParameter("empId", 1);
+            List<Object[]> list = query.list();
+            for (Object[] objects : list) {
+                String name = (String) objects[0];
+                Date doj = (Date) objects[1];
+                Double salary = (Double) objects[2];
+                String city = (String) objects[3];
+                Long pincode = (Long) objects[4];
+                System.out.println(name + "\t" + doj + "\t" + salary + "\t" + city + "\t" + pincode + "\t");
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
