bbfad1c7dc3caab773494ed884b7c83b6286fb19
Abdur Rehman <rizwan.majeed@venturedive.com>
2018-10-02 14:40:16 +0500

    Added First Level Cache Demo

---
+++ Cache/HibernateFirstLevelCache/src/main/java/com/infotech/client/SessionCacheClientTest.java
@@ -0,0 +1,123 @@
+package com.infotech.client;
+
+import com.infotech.entities.Employee;
+import com.infotech.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class SessionCacheClientTest {
+
+    public static void main(String[] args) throws Exception {
+
+//        sessionCacheForInsertRecord();
+//        sesionCacheReadByPrimaryKey();
+//        sesionCacheReadByNaturalId();
+//        sesionCacheForUpdateRecord();
+        sesionCacheForDeleteRecord();
+    }
+
+    private static void sessionCacheForInsertRecord() {
+
+        Transaction tx = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Employee employee = new Employee();
+            employee.setEmployeeName("Ali Ahsan");
+            employee.setUsername("ali");
+            employee.setPassword("ali#123");
+            employee.setAccessLevel(1);
+
+            tx = session.beginTransaction();
+            Long employeeId = (Long) session.save(employee);
+
+            tx.commit();
+
+            System.out.println("-------------------------------------------------------------");
+            Employee employee2 = session.get(Employee.class, employeeId);
+            System.out.println(employee2);
+        } catch (Exception e) {
+            if (tx != null && tx.isActive())
+                tx.rollback();
+            throw e;
+        }
+    }
+
+    private static void sesionCacheReadByPrimaryKey() throws Exception {
+        try(Session session = HibernateUtil.getSessionFactory().openSession() ) {
+            Long employeeId = 1L;
+            Employee employee1 = session.get(Employee.class, employeeId);
+            System.out.println(employee1);
+
+            // Hibernate will issue another query because of empty cache
+//            session.evict(employee1);       // delete a particular object from cache
+//            session.clear();                // delete all objects from cache
+
+            System.out.println("-------------------------------------------------------------");
+            Employee employee2 = session.get(Employee.class, employeeId);
+            System.out.println(employee2);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    private static void sesionCacheReadByNaturalId() throws Exception {
+        try(Session session = HibernateUtil.getSessionFactory().openSession() ) {
+            String username = "ali";
+
+            Employee employee1 = session.bySimpleNaturalId(Employee.class).load(username);
+            System.out.println(employee1);
+
+            System.out.println("-------------------------------------------------------------");
+            Employee employee2 = session.bySimpleNaturalId(Employee.class).load(username);
+            System.out.println(employee2);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+
+    private static void sesionCacheForUpdateRecord() {
+        Transaction tx =  null;
+        try(Session session = HibernateUtil.getSessionFactory().openSession() ) {
+            Long employeeId = 1L;
+            String newPassword = "password$23";
+            Employee employee1 = session.get(Employee.class, employeeId);
+            System.out.println(employee1);
+            if(employee1 != null){
+                tx = session.beginTransaction();
+                employee1.setPassword(newPassword);
+                tx.commit();
+            }
+            System.out.println("-------------------------------------------------------------");
+            Employee employee2 = session.get(Employee.class, employeeId);
+            System.out.println(employee2);
+        } catch (Exception e) {
+            if(tx != null && tx.isActive())
+                tx.rollback();
+            throw e;
+        }
+
+    }
+
+
+    private static void sesionCacheForDeleteRecord() {
+        Transaction tx =  null;
+        try(Session session = HibernateUtil.getSessionFactory().openSession() ) {
+            Long employeeId = 2L;
+            Employee employee1 = session.get(Employee.class, employeeId);
+            System.out.println(employee1);
+            if(employee1 != null){
+                tx = session.beginTransaction();
+                session.delete(employee1);
+                tx.commit();
+            }
+            System.out.println("-------------------------------------------------------------");
+            Employee employee2 = session.get(Employee.class, employeeId);
+            System.out.println(employee2);
+        } catch (Exception e) {
+            if(tx != null && tx.isActive())
+                tx.rollback();
+            throw e;
+        }
+    }
+
+}
